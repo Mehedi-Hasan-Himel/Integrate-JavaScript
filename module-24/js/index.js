c2b465fe8c3f2c196a1b3609c5b7c64a8ee84528
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2023-12-05 15:07:44 +0600

    module 24

--- module-24/js/index.js
+++ module-24/js/index.js
@@ -1,20 +1,30 @@
+// option - 01 - getElementsByTagName
 const liCollection = document.getElementsByTagName("li");
 // console.log(liCollection);
 
 for (const li of liCollection) {
-  console.log(li.innerText);
+  // console.log(li.innerText);
 }
 
 const allHeadings = document.getElementsByTagName("h1");
 for (const h1 of allHeadings) {
-  console.log(h1.innerText);
+  // console.log(h1.innerText);
 }
- 
+//  option - 02 - getElementById
 const fruitsTitle = document.getElementById("fruits-title")
 fruitsTitle.innerText = "Fruit change by js"
 
+//  option - 03 - getElementsByClassName
 const places = document.getElementsByClassName("important-places")
 for (const place of places) {
-   console.log(place.innerText);
+  //  console.log(place.innerText);
 }
 
+//   option - 04 -  querySelector
+//   option - 05 -  querySelectorAll
+
+const someLi = document.querySelectorAll(".fruits-container li");
+// console.log(someLi);
+for (const li of someLi) {
+  console.log(li);
+}
