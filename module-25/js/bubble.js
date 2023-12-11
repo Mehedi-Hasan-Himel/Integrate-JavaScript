0e40e95ea298ec306afa5073277de03712228479
Mehedi-Hasan-Himel <mehedihasanhimel89@gmail.com>
2023-12-11 21:40:00 +0600

    commit
    event delegate and benefit of event bubble

---
+++ module-25/js/bubble.js
@@ -0,0 +1,13 @@
+document.getElementById("item-2").addEventListener("click", function () {
+  console.log("item - 2 licked");
+});
+
+
+document.getElementById("list-ul").addEventListener("click", function () {
+   console.log("list-ul clicked");
+})
+
+document.getElementById("list-container").addEventListener("click", function () {
+   console.log("list-container clicked");
+})
+
---
+++ module-25/js/delegate.js
@@ -0,0 +1,22 @@
+// const items = document.getElementsByClassName("items");
+
+// for (const item of items) {
+//   item.addEventListener("click", function (event) {
+//     console.log(event.target.parentNode.removeChild(event.target));
+//   });
+// }
+
+document
+  .getElementById("list-container")
+  .addEventListener("click", function (event) {
+    event.target.parentNode.removeChild(event.target);
+  });
+
+document.getElementById("btn-add-item").addEventListener("click", function () {
+  const listContainer = document.getElementById("list-container");
+
+  const li = document.createElement("li");
+  li.innerText = "Brand New Item Added.";
+  li.classList.add("item");
+  listContainer.appendChild(li);
+});
